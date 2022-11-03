0c645ecd28808e4dd07e93d842e4b2e9f0ced15b
skapildev <kd3383864@gmail.com>
2022-11-03 17:34:24 +0530

    pull

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Class from "./components/file";
 import Abhi from "./components/hero/btnhero";
 import Application from "./components/apply/apply";
+import Cards from "./components/card/card";
 
 const App = () => {
   return (
     <>
       <Abhi />
+      <Cards />
       <Class />
+
       <Application />
     </>
   );
