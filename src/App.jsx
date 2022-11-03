a1b03f193506ae9c95b9e60404edd73f43642552
Radha02062001 <rajalakshmi96462@gmail.com>
2022-11-03 12:20:10 +0530

    feat(file)

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Class from './components/file';
+
+const App = () => {
+  return <Class/>
+}
+export default App;
---
+++ src/components/file.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './file.css'
+import Image from "../assets/tick.png"
+
+const Class=()=>{
+     return(
+          <div className='fit'>
+          <div className='pro'><h1>Benefits of being a Aadhar Card Delivery Partner</h1>
+          
+         <h2>  <img src={Image}/> Insurance for you and your family</h2>
+   <p>It's very important that our Delivery Partners and their near and dear<br/> ones are cared for and looked after.
+      That is why, <br/>all PDPs and their families are covered under both Medical insurance and Accident Insurance.<br/>
+           The sum total of the insured amounts is Rs.1,00,000 (one lakhs).</p>
+           <h2>  <img src={Image}/> Incredible Incentive</h2>
+           <p>An incentive is a bullet,a key:an often tiny object with astonishing<br/>
+           power to change a situation.I was very excited about a currency that was not controlled<br/>
+           by a central goverment, that could be a free market currency. <br/>
+           That was all the incentive i needed to dedicated my life to it.</p>
+           <h2>  <img src={Image}/> Rewards</h2>
+           <p>One industry that offers an incredible range of perks and incentives to<br/> its employees is software development.
+            Silicon Valley is loaded with companies<br/> that provide everything from free food to massages to its employees.</p>
+            <h2>  <img src={Image}/> Your Second Home</h2>
+            <p> Our partners are our family, and any of the PDPs will tell you about the special moments<br/> 
+            they spend with the our family of managers and fellow PDPs,celebrating major festivals together, <br/>
+            regular meetings to share experiences and building valuable friendships.</p>
+            <h4>So, what are you waiting for? We welcome you to the Our family - join us today!
+
+</h4>
+ </div>
+ </div>
+     )
+}
+export default Class;
