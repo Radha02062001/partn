678d2af62bf62904ec8d2b9d010f4c43fd6d9e3a
vigneshdfc <vigneshdfc02@gmail.com>
2022-11-04 00:21:44 +0530

    feat(partner)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import Class from './components/file';
+import React from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Class from "./components/file";
+import Partner from "./components/review/review";
 
 const App = () => {
-  return <Class/>
-}
+  return (
+    <>
+      <Class />
+      <Partner />
+    </>
+  );
+};
 export default App;
