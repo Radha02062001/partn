b5880b2c468d4f0d4daef537658501411e678657
skapildev <kd3383864@gmail.com>
2022-11-04 13:06:22 +0530

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Class from "./components/file";
 import Abhi from "./components/hero/btnhero";
 import Application from "./components/apply/apply";
 import Cards from "./components/card/card";
-import Head from "./components/head/head"
+import Head from "./components/head/head";
+import Service from "./components/our service/service";
 
 const App = () => {
   return (
     <>
       <Head />
       <Abhi />
-      
       <Class />
       <Cards />
-
+      <Service />
       <Application />
     </>
   );
